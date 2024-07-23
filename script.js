708a1909c16d1adf79fff149deac582aecda13dd
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-23 09:13:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
-
+	const words = document.querySelectorAll("strong");
+	words.forEach((word) => {
+		word.classList.add("green")
+	})
 }
 
 
 function return_normal() {
     //Write your code here
-
+const words = document.querySelectorAll("strong");
+	words.forEach((word) => {
+		word.classList.remove("green")
+	})
     
 }
